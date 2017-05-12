0641dc2b0fe4fe1e14d4291928ccd919207dd23a
Solayter <Anton-nabokin1997@yandex.ru>
2017-05-12 23:57:36 +0300

    Update Memento.cs

--- Tower-Defence v.2/Tower-Defence/Map/Memento.cs
+++ Tower-Defence v.2/Tower-Defence/Map/Memento.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace Tower_Defence
@@ -46,38 +43,33 @@ namespace Tower_Defence
             }
             return file;
         }
-        public static void OpenFile(string open)
+        public static void OpenFile(string open, Gameplay game)
         {
-            FileStream f = new FileStream(open, FileMode.Open, FileAccess.Read);
-            StreamReader f2 = new StreamReader(f);
-            string file = f2.ReadLine();
-            string[] str = file.Split(' ');
-            int a = 0;
-            Map.getInstance().builds.Clear();
-            Map.getInstance().units.Clear();
-
-            Dictionary<string, Action<int,int>> dic = new Dictionary<string, Action<int, int>>();
-
+            using (StreamReader reader = new StreamReader(open))
             {
-                dic.Add("A1", (i, j) => { Map.getInstance().units.Add(Map.getInstance().meleeSpawner.Create(false, i, j)); });
-                dic.Add("A2", (i, j) => { Map.getInstance().units.Add(Map.getInstance().rangeSpawner.Create(false, i, j)); });
-                dic.Add("A3", (i, j) => { Map.getInstance().units.Add(Map.getInstance().ballistaSpawner.Create(false, i, j)); });
-                dic.Add("A4", (i, j) => { Map.getInstance().builds.Add(Map.getInstance().towerSpawner.Create(false, i, j)); });
-                dic.Add("A5", (i, j) => { Map.getInstance().builds.Add(Map.getInstance().castleSpawner.Create(false, i, j)); });
-                dic.Add("E1", (i, j) => { Map.getInstance().units.Add(Map.getInstance().meleeSpawner.Create(true, i, j)); });
-                dic.Add("E2", (i, j) => { Map.getInstance().units.Add(Map.getInstance().rangeSpawner.Create(true, i, j)); });
-                dic.Add("E3", (i, j) => { Map.getInstance().units.Add(Map.getInstance().ballistaSpawner.Create(true, i, j)); });
-                dic.Add("E4", (i, j) => { Map.getInstance().builds.Add(Map.getInstance().towerSpawner.Create(true, i, j)); });
-                dic.Add("E5", (i, j) => { Map.getInstance().builds.Add(Map.getInstance().castleSpawner.Create(true, i, j)); });
-                dic.Add("0",  (i, j) => { Map.getInstance().field[i, j].obj = null; Map.getInstance().field[i, j].pass = true; });
+                string file = reader.ReadLine();
+                string[] str = file.Split(' ');
+                int a = 0;
+                Map.getInstance().builds.Clear();
+                Map.getInstance().units.Clear();
+                Dictionary<string, Action<int, int>> dic = new Dictionary<string, Action<int, int>>();
+                {
+                    dic.Add("A1", (i, j) => { game.SpawnMelee(false, i, j); });
+                    dic.Add("A2", (i, j) => { game.SpawnRange(false, i, j); });
+                    dic.Add("A3", (i, j) => { game.SpawnBallista(false, i, j); });
+                    dic.Add("A4", (i, j) => { game.SpawnTower(false, i, j); });
+                    dic.Add("A5", (i, j) => { game.SpawnCastle(false, i, j); });
+                    dic.Add("E1", (i, j) => { game.SpawnMelee(true, i, j); });
+                    dic.Add("E2", (i, j) => { game.SpawnRange(true, i, j); });
+                    dic.Add("E3", (i, j) => { game.SpawnBallista(true, i, j); });
+                    dic.Add("E4", (i, j) => { game.SpawnTower(true, i, j); });
+                    dic.Add("E5", (i, j) => { game.SpawnCastle(true, i, j); });
+                    dic.Add("0", (i, j) => { Map.getInstance().field[i, j].obj = null; Map.getInstance().field[i, j].pass = true; });
+                }
+                for (int j = 0; j < Map.getInstance().yY; j++)
+                    for (int i = 0; i < Map.getInstance().xX; i++)
+                        dic[str[a++]](i, j);
             }
-
-            for (int j = 0; j < Map.getInstance().yY; j++)
-                for (int i = 0; i < Map.getInstance().xX; i++)
-                    dic[str[a++]](i,j);
-
-            f2.Close();
-            f.Close();
         }
     }
 }
